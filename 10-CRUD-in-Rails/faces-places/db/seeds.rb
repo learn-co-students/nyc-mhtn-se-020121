4777f1f521ef1e5882ef6c450e1afb569745b892
sylwiavargas <sylwia.vargas@gmail.com>
2021-02-24 08:06:15 -0500

    before lecture

--- 10-CRUD-in-Rails/faces-places/db/seeds.rb
+++ 10-CRUD-in-Rails/faces-places/db/seeds.rb
@@ -5,9 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Place.destroy_all
 
-ako_sushi = Place.create(name: "Ako Sushi", street_address: "9478634374 Bleecker Ave", zip: 11237, description: "this is a wonderful place")
+ako_sushi = Place.create(name: "Ako Sushi", street_address: "899 Bleecker Ave", zip: 11237, description: "this is a wonderful place")
+heavenly_scent = Place.create(name: "Heavenly Scent Laundromat", street_address: "1000 Hart St", zip: 11237, description: "a bit dodgy and a health hazard but open 24/7")
 
-heavenly_scent = Place.create(name: "Heavenyl Scent Laundromat", street_address: "1000 Hart St", zip: 11237, description: "a bit dodgy and a health hazard but open 24/7")
+def create_restaurant
+    name = Faker::Restaurant.name
+    description = Faker::Restaurant.description
+    street_address = Faker::Address.street_address
+    zip = Faker::Address.zip
+    Place.create(name: name, description: description, street_address: street_address, zip: zip)
+end
+
+def create_company
+    name_prefix = Faker::Company.name
+    name_suffix = Faker::Company.industry
+    name = name_prefix + " " + name_suffix
+    street_address = Faker::Address.street_address
+    zip = Faker::Address.zip
+    business_prefix = Faker::Company.catch_phrase 
+    business_suffix_1 = Faker::Company.buzzword
+    business_suffix_2 = Faker::Company.buzzword
+    business_motto = Faker::Company.bs 
+    separator = [" | ", " . ", " ~ ", " / ", " ... "].sample
+    description = business_prefix + separator + business_suffix_1 + separator + business_suffix_2 + separator + business_motto
+    Place.create(name: name, street_address: street_address, zip: zip, description: description)
+end
+
+10.times do 
+    create_restaurant
+    create_company
+    create_restaurant
+end
+
 
 puts "🏖 seeding ended"

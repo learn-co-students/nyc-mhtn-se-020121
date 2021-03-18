efb19f5c4ab5fc3bbb04e51f3234513b46ab170d
Michelle Rios <brendamichelle995@gmail.com>
2021-03-18 14:30:16 -0400

    before arrow functions lecture

---
+++ 23-arrow-functions/data.js
@@ -0,0 +1,39 @@
+// array of numbers
+const nums = [4, 66, 9, 77, 90, 10, 33, 1, 23]
+
+// array of objects
+const books = [
+    {
+        title: "We",
+        author: "Zamyatin",
+        yearRead: 2015
+    },
+    {
+        title: "Omon Ra",
+        author: "Pelevin",
+        yearRead: 2017
+    },
+    {
+        title: "Notes From Underground",
+        author: "Dostoevsky",
+        yearRead: 2019
+    },
+    {
+        title: "The Defense",
+        author: "Nabakov",
+        yearRead: 2014
+    },
+    {
+        title: "War and Peace",
+        author: "Tolstoy",
+        yearRead: 2016
+    },
+    {
+        title: "Anna Karenina",
+        author: "Tolstoy",
+        yearRead: 2016
+    }
+]
+
+// array of numbers
+const bookTitles = ["We", "Omon Ra", "The Defense", "Pnin", "War and Peace", "The Foundation Pit", "Notes From Underground"]
---
+++ 23-arrow-functions/index.js
@@ -0,0 +1,33 @@
+function doubleIt(num) {
+    return num * 2
+}
+
+function createPerson(firstName, age) {
+    return {
+        name: firstName,
+        age: age
+    }
+}
+
+
+
+
+/********************** MAP **********************/
+// Transforms an array
+// Does not mutate the original array
+// Callback should return the transformation
+
+
+
+/********************** FILTER **********************/
+// Returns a subarray satisfying a condition
+// Does not mutate the original array
+// Callback should return a boolean
+
+
+
+/********************** SORT **********************/
+// mutates the original array
+// callback should return a number
+
+

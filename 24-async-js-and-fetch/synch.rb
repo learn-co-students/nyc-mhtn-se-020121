24fe2b003320131bb1e620f36af8727c179b55e6
Michelle Rios <brendamichelle995@gmail.com>
2021-03-18 21:36:12 -0400

    before ajax & fetch lecture

---
+++ 24-async-js-and-fetch/synch.rb
@@ -0,0 +1,12 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+# https://randomfox.ca/
+# https://randomfox.ca/floof/
+
+
+puts "Hello World!"
+puts "brb, gonna go sleep for a sec"
+# sleep(5)
+
+puts "What a nice cat nap that was!"

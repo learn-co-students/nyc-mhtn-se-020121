1f24f3fbc00de6d5a3761438bae532606f25f227
Michelle Rios <brendamichelle995@gmail.com>
2021-03-19 15:44:33 -0400

    after fetch CRUD lecture

--- 25-Fetch-CRUD/newsApp/index.js
+++ 25-Fetch-CRUD/newsApp/index.js
@@ -1,3 +1,6 @@
+// PATCH /articles/:id - update an article in the backend & display update on the page
+
+
 /************** GLOBAL VARIABLES **************/
 const newsTitle = document.querySelector('h1#news-co')
 const allCards = document.querySelectorAll('.card')
@@ -20,9 +23,13 @@ function deleteAd() {
 }
 
 function renderAllCards() {
-    articlesArray.forEach(function (articleObject) {
-        renderOneCard(articleObject)
-    })
+    fetch('http://localhost:3000/articles')
+        .then(response => response.json())
+        .then(articles => {
+            articles.forEach(article => {
+                renderOneCard(article)
+            })
+        })
 }
 
 
@@ -63,7 +70,6 @@ toggle.addEventListener('click', function () {
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-    console.log('Form submitted!!!')
 
     // GET THE USER INPUT FROM THE FORM
     const inputFieldOne = event.target[0].value
@@ -76,24 +82,61 @@ form.addEventListener('submit', function (event) {
         author: inputFieldTwo,
         description: inputFieldThree,
         image: inputFieldFour,
-        likes: 0,
-        id: articlesArray[articlesArray.length - 1].id + 1
+        likes: 0
     }
 
-    articlesArray.push(newArticleObject)
-    renderOneCard(newArticleObject)
+    fetch('http://localhost:3000/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(newArticleObject)
+    })
+        .then(response => response.json())
+        .then(newArticle => {
+            // pessimistic rendering - in this case better because we're
+            // wanting the ID when we create the HTML for this card
+            renderOneCard(newArticle)
+        })
+
+    // optimistic rendering
+    // renderOneCard(newArticleObject)
     form.reset()
 })
 
 
 collectionDiv.addEventListener('click', function (event) {
     if (event.target.matches('button.like-button')) {
+        const card = event.target.closest('div.card')
         const likesPtag = event.target.previousElementSibling
-        const likes = parseInt(likesPtag.textContent) + 1
-        likesPtag.textContent = `${likes} Likes`
+        const likesNum = parseInt(likesPtag.textContent) + 1
+        // optimistic rendering
+        likesPtag.textContent = `${likesNum} Likes`
+
+        fetch(`http://localhost:3000/articles/${card.dataset.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ likes: likesNum })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            // .catch()
     }
     else if (event.target.matches('button.delete-button')) {
         const card = event.target.closest('div.card')
+
+
+        fetch(`http://localhost:3000/articles/${card.dataset.id}`, {
+            method: 'DELETE'
+        })
+        // .then(response => response.json())
+        // .then(() => card.remove()) // pessimistic rendering
+
+        // optimistic rendering
         card.remove()
     }
 })
@@ -101,3 +144,4 @@ collectionDiv.addEventListener('click', function (event) {
 
 /************** APP INIT **************/
 renderAllCards()
+deleteAd()

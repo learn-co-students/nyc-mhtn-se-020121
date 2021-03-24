cf7ac23baa1aec753ac0327b430c480a797c7f68
Michelle Rios <brendamichelle995@gmail.com>
2021-03-24 15:13:55 -0400

    after spiceblend review; all deliverables done; no refactor

--- 30-spiceblend-lab-review/src/index.js
+++ 30-spiceblend-lab-review/src/index.js
@@ -1,30 +1,59 @@
 // write your code here
 const url = 'http://localhost:3000/spiceblends'
+const ingredientsUrl = 'http://localhost:3000/ingredients'
 const updateForm = document.querySelector('form#update-form')
 const newIngredientForm = document.querySelector('form#ingredient-form')
+const spiceImagesDiv = document.querySelector('div#spice-images')
+
+
+function displaySpiceBlendDetail(spiceBlendObj) {
+    const detailH2 = document.querySelector('h2.title')
+    detailH2.textContent = spiceBlendObj.title
+
+    const detailImg = document.querySelector('img.detail-image')
+    detailImg.src = spiceBlendObj.image
+    detailImg.alt = spiceBlendObj.title
+
+    updateForm.dataset.id = spiceBlendObj.id
+    newIngredientForm.dataset.id = spiceBlendObj.id
+
+    const ingredientsUl = document.querySelector('ul.ingredients-list')
+
+    ingredientsUl.innerHTML = ''
+
+    spiceBlendObj.ingredients.forEach(ingredientObject => {
+        const li = document.createElement('li')
+        li.textContent = ingredientObject.name
+        ingredientsUl.append(li)
+    })
+}
 
 
 fetch(`${url}/1`)
     .then(response => response.json())
-    .then(data => {
-        const detailH2 = document.querySelector('h2.title')
-        detailH2.textContent = data.title
+    .then(spiceBlendObj => {
+        displaySpiceBlendDetail(spiceBlendObj)
+    })
 
-        const detailImg = document.querySelector('img.detail-image')
-        detailImg.src = data.image
-        detailImg.alt = data.title
 
-        const ingredientsUl = document.querySelector('ul.ingredients-list')
+fetch(url)
+    .then(response => response.json())
+    .then(spiceBlendArray => {
+        spiceBlendArray.forEach(spiceObject => {
+            const img = document.createElement('img')
+            img.src = spiceObject.image
+            img.alt = spiceObject.title
+            img.dataset.id = spiceObject.id
 
-        data.ingredients.forEach(ingredientObject => {
-            const li = document.createElement('li')
-            li.textContent = ingredientObject.name
-            ingredientsUl.append(li)
-
+            spiceImagesDiv.append(img)
         })
     })
 
 
+
+
+
+/********************** EVENT LISTENERS **********************/
 
 updateForm.addEventListener('submit', event => {
     event.preventDefault()
@@ -32,7 +61,7 @@ updateForm.addEventListener('submit', event => {
     // get the user input
     const titleInput = event.target.title.value
 
-    fetch(`${url}/1`, {
+    fetch(`${url}/${event.target.dataset.id}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
@@ -61,6 +90,37 @@ newIngredientForm.addEventListener('submit', event => {
 
     event.target.reset()
 
+    fetch(ingredientsUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: newIngredientInput,
+            spiceblendId: parseInt(event.target.dataset.id)
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+
+})
+
+spiceImagesDiv.addEventListener('click', event => {
+
+    if (event.target.matches('img')) {
+        const id = event.target.dataset.id
+        console.log(id)
+
+        fetch(`${url}/${id}`)
+            .then(response => response.json())
+            .then(spiceBlendObject => {
+                displaySpiceBlendDetail(spiceBlendObject)
+            })
+
+
+    }
 })
 
 

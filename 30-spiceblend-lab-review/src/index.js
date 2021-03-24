7baf88707d49ecc3dfcafa462230d3cb17a2c8b8
Michelle Rios <brendamichelle995@gmail.com>
2021-03-24 13:46:20 -0400

    spiceblend review; deliverable 1 done

--- 30-spiceblend-lab-review/src/index.js
+++ 30-spiceblend-lab-review/src/index.js
@@ -1,1 +1,24 @@
 // write your code here
+const url = 'http://localhost:3000/spiceblends'
+
+
+fetch(`${url}/1`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const detailH2 = document.querySelector('h2.title')
+        detailH2.textContent = data.title
+
+        const detailImg = document.querySelector('img.detail-image')
+        detailImg.src = data.image
+        detailImg.alt = data.title
+
+        const ingredientsUl = document.querySelector('ul.ingredients-list')
+
+        data.ingredients.forEach(ingredientObject => {
+            const li = document.createElement('li')
+            li.textContent = ingredientObject.name
+            ingredientsUl.append(li)
+
+        })
+    })

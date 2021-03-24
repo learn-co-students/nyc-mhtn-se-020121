02957fb5823ee891c712271c65cc937628f61d6d
Michelle Rios <brendamichelle995@gmail.com>
2021-03-24 14:17:08 -0400

    spiceblend review; second & third deliverable done

--- 30-spiceblend-lab-review/src/index.js
+++ 30-spiceblend-lab-review/src/index.js
@@ -1,11 +1,12 @@
 // write your code here
 const url = 'http://localhost:3000/spiceblends'
+const updateForm = document.querySelector('form#update-form')
+const newIngredientForm = document.querySelector('form#ingredient-form')
 
 
 fetch(`${url}/1`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         const detailH2 = document.querySelector('h2.title')
         detailH2.textContent = data.title
 
@@ -22,3 +23,44 @@ fetch(`${url}/1`)
 
         })
     })
+
+
+
+updateForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    // get the user input
+    const titleInput = event.target.title.value
+
+    fetch(`${url}/1`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: titleInput })
+    })
+        .then(response => response.json())
+        .then(updatedSpiceBlendObj => {
+            const detailH2 = document.querySelector('h2.title')
+            detailH2.textContent = updatedSpiceBlendObj.title
+        })
+
+})
+
+
+newIngredientForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    // get the user input
+    const newIngredientInput = event.target['ingredient-name'].value
+    const li = document.createElement('li')
+    li.textContent = newIngredientInput
+
+    const ingredientsUl = document.querySelector('ul.ingredients-list')
+    ingredientsUl.append(li)
+
+    event.target.reset()
+
+})
+
+
